3f1069af4cc6040b3560f5aa8088d2350e07cca7
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-07-29 14:43:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,20 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("comp-score");
 const score_board = document.querySelector("score-board");
 const paper_div = document.getElementById("p");
+main();
 
 /** random computer choice */
 function getComputerChoice(){
     const choice = ["paper","rock","scissors"];
     const result = Math.floor(Math.random()*choice.length);
     console.log(choice[result])
-    computerTurnChoice =choice[result];
-    
+    computerTurnChoice =choice[result];   
 }
 
 function main(){
    paper_div.addEventListener('click', function(){game("paper");})
    rock_div.addEventListener("click",()=>{ game("rock");})
    scissors.addEventListener("click",()=>{ game("scissors");})
-
 }
 
 function game(userChoice){
